61913aec737eab10ba8709f0475c21f693ef0611
Tedu <guohongfeng2468@163.com>
2019-04-25 10:17:27 +0800

    python1

---
+++ day01/run.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+#这是一段注释
+def show(name,age):
+    '''
+    这是测试的方法
+    :param name: 用户姓名
+    :param age: 用户年龄
+    :return: 无
+    '''
+    print("姓名",name,"年龄",age)
+
+
+show("张三",23)
